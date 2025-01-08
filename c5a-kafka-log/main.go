777d9c862097b2bd5022d12dbdb3674ff445d722
Catalin Iosif <catalin.iosif.me@gmail.com>
2025-01-08 20:11:03 +0200

    refactor challenge 5a

--- c5a-kafka-log/main.go
+++ c5a-kafka-log/main.go
@@ -30,145 +30,150 @@ type PollRes struct {
 }
 
 type State struct {
-	Node    *maelstrom.Node
-	Mutex   sync.RWMutex
-	Logs    map[string][]OffsetLog
-	Offsets map[string]int
+	Node       *maelstrom.Node
+	Mutex      sync.RWMutex
+	Logs       map[string][]OffsetLog
+	Offsets    map[string]int
+	OffsetBase int
 }
 
 func main() {
 	n := maelstrom.NewNode()
 	s := &State{
-		Node:    n,
-		Logs:    make(map[string][]OffsetLog),
-		Offsets: make(map[string]int),
+		Node:       n,
+		Logs:       make(map[string][]OffsetLog),
+		Offsets:    make(map[string]int),
+		OffsetBase: 1000,
 	}
 
-	const offset_base = 1000
-
-	n.Handle("send", func(msg maelstrom.Message) error {
-
-		var body map[string]any
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err
-		}
-
-		key := body["key"].(string)
-		message := body["msg"].(float64)
-
-		s.Mutex.Lock()
-
-		len := len(s.Logs[key]) + offset_base
-		s.Logs[key] = append(s.Logs[key], OffsetLog{len, int(message)})
-
-		s.Mutex.Unlock()
-
-		body["type"] = "send_ok"
-		body["offset"] = len
-		delete(body, "msg")
-		delete(body, "key")
-
-		return n.Reply(msg, body)
-	})
-
-	n.Handle("poll", func(msg maelstrom.Message) error {
-
-		var body OffsetReq
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err
-		}
-
-		messages := make(map[string][]OffsetLog)
-
-		s.Mutex.Lock()
-
-		if body.Offsets != nil {
-			for key, value := range body.Offsets {
-				logs_for_key := s.Logs[key]
-				if len(logs_for_key) != 0 {
-					filtered_logs := []OffsetLog{}
-
-					start := int(value) - offset_base
-					if start < 0 {
-						start = 0
-					}
-					for i := start; i < len(logs_for_key); i++ {
-						filtered_logs = append(filtered_logs, logs_for_key[i])
-					}
-					messages[key] = filtered_logs
-				}
-			}
-		} else {
-			for key, value := range s.Logs {
-				messages[key] = value
-			}
-		}
-		s.Mutex.Unlock()
-
-		res := PollRes{
-			Type:      "poll_ok",
-			Messages:  messages,
-			MessageId: body.MessageId,
-		}
-		return n.Reply(msg, res)
-	})
-
-	n.Handle("commit_offsets", func(msg maelstrom.Message) error {
-		var body OffsetReq
-
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err
-		}
-
-		req_offsets := body.Offsets
-		s.Mutex.Lock()
-		for key, value := range req_offsets {
-			entry := s.Offsets[key]
-			if entry != 0 {
-				if int(value) < entry {
-					continue
-				}
-			} else {
-				s.Offsets[key] = int(value)
-			}
-		}
-		s.Mutex.Unlock()
-		res := make(map[string]any)
-		res["type"] = "commit_offsets_ok"
-		res["msg_id"] = body.MessageId
-
-		return n.Reply(msg, res)
-	})
-
-	n.Handle("list_committed_offsets", func(msg maelstrom.Message) error {
-		var body KeysReq
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err
-		}
-
-		keys := body.Keys
-		res_offsets := make(map[string]int)
-
-		s.Mutex.RLock()
-		for _, key := range keys {
-			entry := s.Offsets[key]
-			if entry != 0 {
-				res_offsets[key] = entry
-			}
-
-		}
-		s.Mutex.RUnlock()
-		result := make(map[string]any)
-		result["offsets"] = res_offsets
-		result["type"] = "list_committed_offsets_ok"
-		result["msg_id"] = body.MessageId
-
-		return n.Reply(msg, result)
-	})
+	n.Handle("send", s.handle_send)
+	n.Handle("poll", s.handle_poll)
+	n.Handle("commit_offsets", s.handle_commit_offset)
+	n.Handle("list_committed_offsets", s.handle_list_committed_offsets)
 
 	// Execute the node's message loop. This will run until STDIN is closed.
 	if err := n.Run(); err != nil {
 		log.Printf("ERROR: %s", err)
 		os.Exit(1)
 	}
+}
+
+func (s *State) handle_send(msg maelstrom.Message) error {
+
+	var body map[string]any
+	if err := json.Unmarshal(msg.Body, &body); err != nil {
+		return err
+	}
+
+	key := body["key"].(string)
+	message := body["msg"].(float64)
+
+	s.Mutex.Lock()
+
+	len := len(s.Logs[key]) + s.OffsetBase
+	s.Logs[key] = append(s.Logs[key], OffsetLog{len, int(message)})
+
+	s.Mutex.Unlock()
+
+	body["type"] = "send_ok"
+	body["offset"] = len
+	delete(body, "msg")
+	delete(body, "key")
+
+	return s.Node.Reply(msg, body)
+}
+
+func (s *State) handle_poll(msg maelstrom.Message) error {
+
+	var body OffsetReq
+	if err := json.Unmarshal(msg.Body, &body); err != nil {
+		return err
+	}
+
+	messages := make(map[string][]OffsetLog)
+
+	s.Mutex.Lock()
+
+	if body.Offsets != nil {
+		for key, value := range body.Offsets {
+			logs_for_key := s.Logs[key]
+			if len(logs_for_key) != 0 {
+				filtered_logs := []OffsetLog{}
+
+				start := int(value) - s.OffsetBase
+				if start < 0 {
+					start = 0
+				}
+				for i := start; i < len(logs_for_key); i++ {
+					filtered_logs = append(filtered_logs, logs_for_key[i])
+				}
+				messages[key] = filtered_logs
+			}
+		}
+	} else {
+		for key, value := range s.Logs {
+			messages[key] = value
+		}
+	}
+	s.Mutex.Unlock()
+
+	res := PollRes{
+		Type:      "poll_ok",
+		Messages:  messages,
+		MessageId: body.MessageId,
+	}
+	return s.Node.Reply(msg, res)
+}
+
+func (s *State) handle_commit_offset(msg maelstrom.Message) error {
+	var body OffsetReq
+
+	if err := json.Unmarshal(msg.Body, &body); err != nil {
+		return err
+	}
+
+	req_offsets := body.Offsets
+	s.Mutex.Lock()
+	for key, value := range req_offsets {
+		entry := s.Offsets[key]
+		if entry != 0 {
+			if int(value) < entry {
+				continue
+			}
+		} else {
+			s.Offsets[key] = int(value)
+		}
+	}
+	s.Mutex.Unlock()
+	res := make(map[string]any)
+	res["type"] = "commit_offsets_ok"
+	res["msg_id"] = body.MessageId
+
+	return s.Node.Reply(msg, res)
+}
+
+func (s *State) handle_list_committed_offsets(msg maelstrom.Message) error {
+	var body KeysReq
+	if err := json.Unmarshal(msg.Body, &body); err != nil {
+		return err
+	}
+
+	keys := body.Keys
+	res_offsets := make(map[string]int)
+
+	s.Mutex.RLock()
+	for _, key := range keys {
+		entry := s.Offsets[key]
+		if entry != 0 {
+			res_offsets[key] = entry
+		}
+
+	}
+	s.Mutex.RUnlock()
+	result := make(map[string]any)
+	result["offsets"] = res_offsets
+	result["type"] = "list_committed_offsets_ok"
+	result["msg_id"] = body.MessageId
+
+	return s.Node.Reply(msg, result)
 }
